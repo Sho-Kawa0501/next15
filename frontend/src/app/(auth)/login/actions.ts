5f6748c7425f8c3b2ee09602e04dd129dfd95d67
Sho-Kawa0501 <akirasho0501@gmail.com>
2025-11-26 09:43:45 +0900

    LoginActions リダイレクト先変更、 インデント修正等

--- frontend/src/app/(auth)/login/actions.ts
+++ frontend/src/app/(auth)/login/actions.ts
@@ -1,26 +1,20 @@
 'use server'
 
-import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-
 import { createClient } from '@/utils/supabase/server'
 
 export async function login() {
-  console.log("google login")
-  //googleログイン
-  // 非同期で
   const supabase = await createClient()
   const { data, error } = await supabase.auth.signInWithOAuth({
-  provider: "google",
-  options: {
-    redirectTo: 'http://localhost:3000//auth/callback',
-  },
-})
+    provider: "google",
+    options: {
+      redirectTo: 'https://www.next-food-delivery.com//auth/callback',
+    },
+  })
 
-if (data.url) {
-  redirect(data.url) // use the redirect API for your server framework
-}
-}
+  if (data.url) {
+    redirect(data.url)
+  }}
 
 export async function logout() {
   const supabase = await createClient()
